35f31e4dd84172d7035ac42d3c4a1eea3b108e67
edochiesamson <edochiesamson827@gmail.com>
2024-07-02 15:18:17 +0000

    Samson

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Samsom"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "SamChain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SamToken"
 )
 
 var ID ids.ID
